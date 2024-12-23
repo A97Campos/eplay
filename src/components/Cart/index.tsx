422bd268d206dc94f56bc07af5ebb875cf34d2d1
A97Campos <andrewleandro97@gmail.com>
2024-12-23 18:00:51 -0300

    concluindo o carrinho

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,30 +1,50 @@
 import { Tags } from '../Tags'
 import starWars from '../../assets/images/star_wars.png'
-import { CartContainer, Overlay, Sidebar } from './style'
+import {
+  CartContainer,
+  Overlay,
+  Sidebar,
+  CartItem,
+  Prices,
+  Quantity
+} from './style'
+import { Button } from '../Button'
 
 export const Cart = () => {
   return (
     <CartContainer>
       <Overlay />
       <Sidebar>
+        <ul>
+          <CartItem>
+            <img src={starWars} />
+            <div>
+              <h3>nome do jogo</h3>
+              <Tags>RPG</Tags>
+              <Tags>PS5</Tags>
+              <p>R$ 190,90</p>
+            </div>
+          </CartItem>
+          <CartItem>
+            <img src={starWars} />
+            <div>
+              <h3>nome do jogo</h3>
+              <Tags>RPG</Tags>
+              <Tags>PS5</Tags>
+              <p>R$ 190,90</p>
+            </div>
+          </CartItem>
+        </ul>
         <div>
-          <ul>
-            <li>
-              <img src={starWars} />
-              <div>
-                <h3>nome do jogo</h3>
-                <Tags>RPG</Tags>
-                <Tags>PS5</Tags>
-                <p>R$ 190,90</p>
-              </div>
-            </li>
-          </ul>
+          <Quantity>2 jogo(s) no carrinho</Quantity>
+          <Prices>
+            Total de R$ 381,80
+            <span>em até 6x sem juros</span>
+          </Prices>
         </div>
-        <p>2 jogo(s) no carrinho</p>
-        <p>
-          Total de R$ 381,80
-          <span>em até 6x sem juros</span>
-        </p>
+        <Button type="button" title="Continuar a compra">
+          Continuar a compra
+        </Button>
       </Sidebar>
     </CartContainer>
   )
--- src/components/Cart/style.ts
+++ src/components/Cart/style.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 import { cores } from '../../styles'
+import { ButtonContainer } from '../Button/style'
+import { TagsContainer } from '../Tags/style'
 
 export const Overlay = styled.div`
   position: absolute;
@@ -18,11 +20,48 @@ export const CartContainer = styled.div`
   width: 100%;
   height: 100%;
   z-index: 1;
+  display: flex;
+  justify-content: end;
 `
 
 export const Sidebar = styled.aside`
+  padding: 40px 16px;
   background-color: ${cores.cinza};
   height: 100%;
   width: 360px;
   z-index: 1;
+
+  ${ButtonContainer} {
+    width: 100%;
+    margin-top: 24px;
+  }
+`
+
+export const CartItem = styled.li`
+  display: flex;
+  border-bottom: 1px solid ${cores.cinzaClaro};
+  padding: 8px 0;
+
+  img {
+    height: 80px;
+    width: 80px;
+    margin-right: 23px;
+  }
+
+  ${TagsContainer} {
+    margin: 8px 0 16px 0;
+  }
+`
+export const Quantity = styled.p`
+  margin: 32px 0 16px 0;
+  font-size: 16px;
+`
+
+export const Prices = styled.p`
+  font-size: 14px;
+  span {
+    display: block;
+    font-size: 12px;
+    color: ${cores.cinzaClaro};
+  }
 `
